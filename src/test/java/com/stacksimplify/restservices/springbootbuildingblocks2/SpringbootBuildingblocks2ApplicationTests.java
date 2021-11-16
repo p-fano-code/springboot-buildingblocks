9f5e297a8f4dc7720067e44c53f4db219c450fd2
Pelayo Fano <pelayofano@gmail.com>
2021-11-16 11:59:48 +0100

    first commit

---
+++ src/test/java/com/stacksimplify/restservices/springbootbuildingblocks2/SpringbootBuildingblocks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.springbootbuildingblocks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
